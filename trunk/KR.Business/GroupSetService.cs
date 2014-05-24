fb245788fa2fc0faec7df83b2b9b0301e2930028
MinhTQSE61009@fpt.edu.vn <MinhTQSE61009@fpt.edu.vn@2ac4d703-09d3-15a6-0012-618bb09a15cd>
2014-05-24 07:53:53 +0000

    Initial Import

---
+++ trunk/KR.Business/GroupSetService.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KR.DataAccess;
+
+namespace KR.Business
+{
+    public class GroupSetService
+    {
+        public static List<GroupSet> GetAll()
+        {
+            return GroupSetDAO.GetAll();
+        }
+
+
+        public static bool AddGroup(GroupSet gro)
+        {
+            return GroupSetDAO.AddGroup(gro);
+        }
+
+
+        public static bool UpdateGroup(GroupSet gro)
+        {
+            return GroupSetDAO.UpdateGroup(gro);
+        }
+
+
+        public static bool RemoveGroup(GroupSet gro)
+        {
+            return GroupSetDAO.RemoveGroup(gro);
+        }
+
+
+        public static GroupSet GetGroupByID(int id)
+        {
+            return GroupSetDAO.GetGroupByID(id);
+        }
+
+
+        public static GroupSet GetGroupByName(string name)
+        {
+            return GroupSetDAO.GetGroupByName(name);
+        }
+
+
+        public static List<GroupSet> GetGroupsStartsName(string name)
+        {
+            return GroupSetDAO.GetGroupsStartsName(name);
+        }
+
+
+        public static List<GroupSet> GetGroupsContainsName(string name)
+        {
+            return GroupSetDAO.GetGroupsContainsName(name);
+        }
+
+
+        public static List<GroupSet> GetGroupsOnTop(int amount)
+        {
+            return GroupSetDAO.GetGroupsOnTop(amount);
+        }
+
+
+        public static List<GroupSet> GetGroupsSkipAmount(int skipAmount, int takeAmount)
+        {
+            return GetGroupsSkipAmount(skipAmount, takeAmount);
+        }
+
+
+        public static GroupSet GetPreviousGroup(int currentGroupID)
+        {
+            return GroupSetDAO.GetPreviousGroup(currentGroupID);
+        }
+
+
+        public static GroupSet GetNextGroup(int currentGroupID)
+        {
+            return GroupSetDAO.GetNextGroup(currentGroupID);
+        }
+
+
+        public static GroupSet GetRandomGroup()
+        {
+            return GroupSetDAO.GetRandomGroup();
+        }
+    }
+}
---
+++ trunk/KR.Business/LesssonService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KR.DataAccess;
+
+namespace KR.Business
+{
+    public class LesssonService
+    {
+        public static List<string> GetAllLessonName()
+        {
+            return LessonDAO.GetAllLessonName();
+        }
+    }
+}
